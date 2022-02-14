e0041457fd31b905f996e1b02c03da3404b35d3a
DariuszGarbarz <plgarbarda@gmail.com>
2022-02-14 16:38:01 +0100

    CodeBehind Completed

--- ElaCompilBleBox/MainWindow.xaml.cs
+++ ElaCompilBleBox/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WLightBoxApi.Contracts;
+using WLightBoxApi.WebServices;
+using System.Drawing;
 
 namespace ElaCompilBleBox
 {
@@ -23,6 +17,209 @@ namespace ElaCompilBleBox
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+        private void Handle_Checked_Color(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            this.ColorModeToSet.Text = (string)rb.Content;
+        }
+
+        private void Handle_Checked_Effect(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            this.EffectModeToSet.Text = (string)rb.Content;
+        }
+
+        private void ColorFadeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = Convert.ToInt32(e.NewValue);
+            string msg = String.Format("Current value: {0}", val);
+            this.ColorFadeValue.Text = msg;
+
+        }
+
+        private void EffectFadeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = Convert.ToInt32(e.NewValue);
+            string msg = String.Format("Current value: {0}", val);
+            this.EffectFadeValue.Text = msg;
+        }
+
+        private void EffectStepSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = Convert.ToInt32(e.NewValue);
+            string msg = String.Format("Current value: {0}", val);
+            this.EffectStepValue.Text = msg;
+        }
+
+        private void RedStepSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = Convert.ToInt32(e.NewValue);
+            string msg = String.Format("Current value: {0}", val);
+            this.RedValue.Text = msg;
+
+            var redValue = Convert.ToInt32(this.RedStepSlider.Value);
+            var greenValue = Convert.ToInt32(this.GreenStepSlider.Value);
+            var blueValue = Convert.ToInt32(this.BlueStepSlider.Value);
+            System.Drawing.Color myColor = System.Drawing.Color.FromArgb(redValue, greenValue, blueValue);
+            string hex = myColor.R.ToString("X2") + myColor.G.ToString("X2") + myColor.B.ToString("X2");
+
+            SetColor.Text = $"{hex}0000";
+
+            var converter = new BrushConverter();          
+            var brush = (Brush)converter.ConvertFromString($"#{hex}");
+            this.SetColorBox.Background = brush;
+
+        }
+
+        private void GreenStepSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = Convert.ToInt32(e.NewValue);
+            string msg = String.Format("Current value: {0}", val);
+            this.GreenValue.Text = msg;
+
+            var redValue = Convert.ToInt32(this.RedStepSlider.Value);
+            var greenValue = Convert.ToInt32(this.GreenStepSlider.Value);
+            var blueValue = Convert.ToInt32(this.BlueStepSlider.Value);
+            System.Drawing.Color myColor = System.Drawing.Color.FromArgb(redValue, greenValue, blueValue);
+            string hex = myColor.R.ToString("X2") + myColor.G.ToString("X2") + myColor.B.ToString("X2");
+
+            SetColor.Text = $"{hex}0000";
+
+            var converter = new BrushConverter();
+            var brush = (Brush)converter.ConvertFromString($"#{hex}");
+            this.SetColorBox.Background = brush;
+        }
+
+        private void BlueStepSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = Convert.ToInt32(e.NewValue);
+            string msg = String.Format("Current value: {0}", val);
+            this.BlueValue.Text = msg;
+
+            var redValue = Convert.ToInt32(this.RedStepSlider.Value);
+            var greenValue = Convert.ToInt32(this.GreenStepSlider.Value);
+            var blueValue = Convert.ToInt32(this.BlueStepSlider.Value);
+            System.Drawing.Color myColor = System.Drawing.Color.FromArgb(redValue, greenValue, blueValue);
+            string hex = myColor.R.ToString("X2") + myColor.G.ToString("X2") + myColor.B.ToString("X2");
+
+            SetColor.Text = $"{hex}0000";
+
+            var converter = new BrushConverter();
+            var brush = (Brush)converter.ConvertFromString($"#{hex}");
+            this.SetColorBox.Background = brush;
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient httpClient = HttpClientSetup.CreateHttpClient();
+            string ipAdress = this.DeviceAdress.Text;
+            GetInfo getInfo = new GetInfo(ipAdress, httpClient);
+            DeviceResponse deviceStatus = getInfo.GetInfoFromApi();
+
+            this.DeviceName.Text = deviceStatus.device.deviceName;
+            this.ProductName.Text = deviceStatus.device.product;
+            this.Hv.Text = deviceStatus.device.hv;
+            this.Fv.Text = deviceStatus.device.fv;
+            this.ActualDeviceAdress.Text = deviceStatus.device.ip;
+
+            GetRgbw getRgbw = new GetRgbw(ipAdress, httpClient);
+            RgbwContract rgbwStatus = getRgbw.GetRgbwFromApi();
+
+            this.ActualColorMode.Text = ColorModes[rgbwStatus.rgbw.colorMode];
+            this.ActualEffectId.Text = ColorEffects[rgbwStatus.rgbw.effectID];
+            this.ActualColorFade.Text = Convert.ToString(rgbwStatus.rgbw.durationsMs.colorFade);
+            this.ActualEffectFade.Text = Convert.ToString(rgbwStatus.rgbw.durationsMs.effectFade);
+            this.ActualEffectStep.Text = Convert.ToString(rgbwStatus.rgbw.durationsMs.effectStep);
+            this.ActualColor.Text = Convert.ToString(rgbwStatus.rgbw.currentColor);
+
+            var converter = new BrushConverter();
+            
+            string actualColor = Convert.ToString(rgbwStatus.rgbw.currentColor);
+            var brush = (Brush)converter.ConvertFromString($"#{actualColor}");
+            this.ActualColorBox.Background = brush;
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            RgbwContract rgbwContract = new RgbwContract();
+
+            rgbwContract.rgbw.currentColor = "----------";
+            rgbwContract.rgbw.lastOnColor = "----------";
+            rgbwContract.rgbw.desiredColor = this.SetColor.Text;
+
+            rgbwContract.rgbw.durationsMs.colorFade = Convert.ToInt32(this.ColorFadeSlider.Value);
+            rgbwContract.rgbw.durationsMs.effectFade = Convert.ToInt32(this.EffectFadeSlider.Value);
+            rgbwContract.rgbw.durationsMs.effectStep = Convert.ToInt32(this.EffectStepSlider.Value);
+
+            rgbwContract.rgbw.colorMode = Int32.Parse(this.ColorModeToSet.Text.Substring(0, 1));
+            rgbwContract.rgbw.effectID = Int32.Parse(this.EffectModeToSet.Text.Substring(0, 1));
+
+            HttpClient httpClient = HttpClientSetup.CreateHttpClient();
+            string ipAdress = this.DeviceAdress.Text;
+
+            PostRgbw postRgbw = new PostRgbw(ipAdress, rgbwContract, httpClient);
+            RgbwContract rgbwStatus = postRgbw.PostRgbwToApi();
+
+            this.ActualColorMode.Text = ColorModes[rgbwStatus.rgbw.colorMode];
+            this.ActualEffectId.Text = ColorEffects[rgbwStatus.rgbw.effectID];
+            this.ActualColorFade.Text = Convert.ToString(rgbwStatus.rgbw.durationsMs.colorFade);
+            this.ActualEffectFade.Text = Convert.ToString(rgbwStatus.rgbw.durationsMs.effectFade);
+            this.ActualEffectStep.Text = Convert.ToString(rgbwStatus.rgbw.durationsMs.effectStep);
+            this.ActualColor.Text = Convert.ToString(rgbwStatus.rgbw.currentColor);
+
+            var converter = new BrushConverter();
+
+            string actualColor = Convert.ToString(rgbwStatus.rgbw.currentColor);
+            var brush = (Brush)converter.ConvertFromString($"#{actualColor}");
+            this.ActualColorBox.Background = brush;
+
+
+        }
+
+        public string[] ColorModes = new string[] {"OFF",
+            "RGBW",
+            "RGB",
+            "MONO",
+            "RGBorW",
+            "CT",
+            "CTx2",
+            "RGBWW"};
+
+        public string[] ColorEffects = new string[] {"OFF",
+            "Fade",
+            "RGB",
+            "Police",
+            "Relax",
+            "Strobo",
+            "Bell" };
+
+        public enum ColorModeEnum
+        {
+            RGBW = 1,
+            RGB,
+            MONO,
+            RGBorW,
+            CT,
+            CTx2,
+            RGBWW
+        }
+
+        public enum EffectModeEnum
+        {
+            OFF,
+            Fade,
+            RGB,
+            Police,
+            Relax,
+            Strobo,
+            Bell
+        }
+
+      
+
+        
     }
 }
